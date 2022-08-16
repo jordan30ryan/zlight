b3cbcc47ca6c514d562cfec79516f0eaa3bcb9fc
Jordan Ross <jordanathew@gmail.com>
2022-08-16 21:06:12 +0100

    itest 23w

--- client/client.py
+++ client/client.py
@@ -1,17 +1,24 @@
 import requests
 
 URL = "192.168.6.154"
-PORT = "5000"
+PORT = ":5000"
 
 # get light status
 def get_light_color():
     url = "http://" + URL + PORT
     request = url + "/color"
     r = requests.get(request)
-    print(r)
+    print(r.text)
 
 # set light status
 # TODO
+def set_light_color(color):
+    url = "http://" + URL + PORT
+    request = url + "/color"
+    color.
+    r = requests.post(request, )
+    print(r.text)
 
 if __name__ == "__main__":
+    set_light_color(21)
     get_light_color()
