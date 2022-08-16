8efb09a615d3ad60afd3cb6d94c36dc074b4c089
Jordan Ross <jordanathew@gmail.com>
2022-08-16 21:14:25 +0100

    exception

--- client/client.py
+++ client/client.py
@@ -8,8 +8,13 @@ PORT = ":5000"
 def get_light_color():
     url = "http://" + URL + PORT
     request = url + "/color"
-    r = requests.get(request)
-    print(r.text)
+
+    try:
+        r = requests.get(request)
+        print(r.text)
+    except:
+        print('Something went wrong')
+
 
 # set light status
 # TODO
@@ -18,9 +23,14 @@ def set_light_color(color_num):
     request = url + "/color"
     params = { 'color' : color_num }
     params_json = json.dumps(params)
-    r = requests.post(request, params_json)
-    print(r.text)
+
+    try:
+        r = requests.post(request, params_json)
+        print(r.text)
+    except:
+        print('Something went wrong')
+
 
 if __name__ == "__main__":
-    set_light_color(21)
+    set_light_color(150)
     get_light_color()
