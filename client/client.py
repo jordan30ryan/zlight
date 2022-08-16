cc705ed0fc028217f7522171423a74bc13e70f4a
Jordan Ross <jordan@tadpol.life>
2022-08-16 13:20:46 -0500

    Test client

--- client/client.py
+++ client/client.py
@@ -1,6 +1,13 @@
+import requests
 
-
+URL = "192.168.6.154"
+PORT = "5000"
 
 # get light status
+def get_light_color():
+    url = "http://" + URL + PORT
+    request = url + "/color"
+    r = requests.get(request)
 
 # set light status
+# TODO
