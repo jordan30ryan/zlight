4027111267bbb0ca56558f8ee2398f0010058e05
Jordan Ross <jordan@tadpol.life>
2022-08-16 09:06:09 -0500

    Remove db references

--- src/server.py
+++ src/server.py
@@ -1,15 +1,7 @@
 from flask import Flask
 from flask import request
-import mysql.connector
 
 app = Flask(__name__)
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="mysql",
-  password="test123",
-  database="zlight"
-)
 
 COLOR = { 'color' : 0 }
 
@@ -29,18 +21,10 @@ def manage_color():
 
 def db_get_color():
     return COLOR['color']
-#    mycursor = mydb.cursor()
-#    mycursor.execute("SELECT color FROM light_color;")
-#    return mycursor.fetchone()[0]
-
 
 def db_set_color(new_color):
     COLOR['color'] = new_color
     return
-#    mycursor = mydb.cursor()
-#    mycursor.execute("UPDATE light_color SET color=%s;", (color,))
-#    return
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
