d802b64719df13f85967372cc368f13fb0b6b926
Jordan Ross <jordanathew@gmail.com>
2022-08-21 13:58:38 -0500

    button

--- client/client.py
+++ client/client.py
@@ -9,9 +9,10 @@ URL = '192.168.6.154'
 PORT = ':5000'
 
 # Push button input settings
+ButtonChannel = 7
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(ButtonChannel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
 
 # TODO light output settings
 
@@ -33,19 +34,16 @@ def change_light_color():
     try:
         r = requests.post(request)
         print(r.text)
-    except Exception as e:
-        print('error sending light color')
+    except Exception as e: print('error sending light color')
         print(e)
 
 def get_button_input():
-    while True:
-        try:
-            if GPIO.input(10) == GPIO.HIGH:
-                print('Button was pushed!')
-                change_light_color()
-        except Exception as e:
-            print('error in get_button_input')
-            print(e)
+    try:
+        print('Button was pushed!')
+        change_light_color()
+    except Exception as e:
+        print('error in get_button_input')
+        print(e)
 
 def set_light(): 
     while True:
@@ -60,11 +58,11 @@ def set_light():
 
 
 def main():
-#    change_light_color()
-    t1 = threading.Thread(target=get_button_input)
+    GPIO.add_event_detect(ButtonChannel, GPIO.RISING, callback=get_button_input, bouncetime=200)
+    #t1 = threading.Thread(target=get_button_input)
     t2 = threading.Thread(target=set_light)
 
-    t1.start()
+    #t1.start()
     t2.start()
 
 
